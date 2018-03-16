aaa060bf83dc10da43f5902f91946d8e4fd4ee8e
clarencec <tong901@163.com>
2018-03-16 09:41:37 +0800

    ES6 demo

--- Demos/Use_ES6/webpack.config.js
+++ Demos/Use_ES6/webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = {
     entry: './main.js',
     output: {
-        filname: 'bundle.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, './dist'),
     },
     module: {
