b4d11e4131a5488661991d9fb3bb9ab7083c16de
clarencec <tong901@163.com>
2018-03-22 22:20:31 +0800

    TypeScript demo

---
+++ Demos/Use_TypeScript/show.ts
@@ -0,0 +1,8 @@
+// show.ts
+// 操作 DOM 元素，把 content 显示到网页上
+// 通过 ES6 模块规范展出 show 函数
+// 给 show 函数增加类型检查
+export function show(content: string) {
+    window.document.getElementById('app').innerText = 'Hello,' + content
+}
+
