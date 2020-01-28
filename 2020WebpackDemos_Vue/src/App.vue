4dfa940651ddf5c5dad9364f2a92b762308fe149
ClarenceChan <tong901@163.com>
2020-01-28 22:16:28 +0800

    vue  demos

---
+++ 2020WebpackDemos_Vue/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id='container'>
+    <h1>{{initData}}</h1>
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return {
+      initData: 'Vue开发环境运行成功'
+    }
+  }
+}
+</script>
+<style scoped>
+  #container{
+    color: red;
+    display: flex;
+  }
+</style>
