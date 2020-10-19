c2db8d1d4880d15dae8154c92480e2a05efa5871
Clarence.C <tong901@163.com>
2020-10-19 11:52:13 +0800

    Lazy Loading

--- webpack5Guide/src/index.js
+++ webpack5Guide/src/index.js
@@ -1,26 +1,48 @@
+// import _ from 'lodash'
+// import printMe from './print.js'
+
+
+// function component() {
+//   const element = document.createElement('div')
+//   const btn = document.createElement('button')
+
+//   element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+
+//   btn.innerHTML = 'Click me and check the console!'
+//   btn.onclick = printMe
+
+//   element.appendChild(btn)
+
+//   return element
+// }
+
+// if (module.hot) {
+//   module.hot.accept('./print.js', function() {
+//     console.log('Accepting the updated printMe module!')
+//     printMe()
+//   })
+// }
+
+// document.body.appendChild(component())
+
 import _ from 'lodash'
-import printMe from './print.js'
 
-
-function component() {
+function component () {
   const element = document.createElement('div')
-  const btn = document.createElement('button')
+  const button = document.createElement('button')
+  const br = document.createElement('br')
 
+  button.innerHTML = 'Click me and look at the console!'
   element.innerHTML = _.join(['Hello', 'webpack'], ' ')
 
-  btn.innerHTML = 'Click me and check the console!'
-  btn.onclick = printMe
+  element.appendChild(br)
+  element.appendChild(button)
 
-  element.appendChild(btn)
-
-  return element
-}
-
-if (module.hot) {
-  module.hot.accept('./print.js', function() {
-    console.log('Accepting the updated printMe module!')
-    printMe()
+  button.onclick = e => import(/* webpackChunkName: "print" */ './print').then(module => {
+    const print = module.default
+    print()
   })
+  return element
 }
 
 document.body.appendChild(component())
--- webpack5Guide/src/print.js
+++ webpack5Guide/src/print.js
@@ -1,3 +1,5 @@
-export default function print() {
-  console.log('Updating print.js...')
+console.log('The print.js module has loaded! See the network tab in dev tools...')
+
+export default () => {
+  console.log('Button Clicked: Here\'s "some text"')
 }
